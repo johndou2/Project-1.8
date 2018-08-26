42803e97871ed09f1a05ebf0d353b7839627ada2
Doug Johnson <johndou2@isu.edu>
2018-08-25 21:31:13 -0600

    First commit of previously created project.

---
+++ src/AreaAndPermiterofAcircle.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class AreaAndPermiterofAcircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//create variable for pi
+		final double PI = 3.14159;
+		//double radius = 5.5;
+		
+        
+        //create scanner project
+        Scanner input = new Scanner (System.in);
+        System.out.println("Please enter a number for a radius. ");
+		double radius = input.nextDouble();
+		double perimeter = 2 * radius * PI;
+        double area = radius * radius * PI;
+        
+        System.out.println("For your given cirlce, ");
+		System.out.println("The permiter is:" +perimeter + ".");
+		System.out.println("And the area is: " + area + ".");
+	}
+
+}
